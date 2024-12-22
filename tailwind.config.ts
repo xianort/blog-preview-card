7ab02504fe41e1285e912149a5bab4f3b91534d8
xianort <xianort99@gmail.com>
2024-12-22 16:41:14 +0300

    Define project styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,42 @@ import type { Config } from "tailwindcss";
 export default {
     content: ["./src/**/*.{html,htm}"],
     theme: {
-        extend: {},
+        colors: {
+            yellow: "hsl(47, 88%, 63%)",
+            white: "hsl(0, 0%, 100%)",
+            grey: {
+                500: "hsl(0, 0%, 42%)",
+                950: "hsl(0, 0%, 7%)",
+            },
+        },
+        fontFamily: {
+            sans: ["Figtree", "FigtreeStatic", "sans-serif"],
+        },
+        borderRadius: {
+            none: "0",
+            sm: "4px",
+            DEFAULT: "10px",
+            lg: "20px",
+        },
+        boxShadow: {
+            DEFAULT: "8px 8px hsl(0, 0%, 0%)",
+        },
+        extend: {
+            keyframes: {
+                intro: {
+                    from: {
+                        transform: "scale(0.96)",
+                        opacity: "0",
+                    },
+                    to: {
+                        transform: "scale(1)",
+                        opacity: "1",
+                    },
+                },
+            },
+            animation: {
+                intro: "intro 200ms ease-out 300ms backwards",
+            },
+        },
     },
-    plugins: [],
 } satisfies Config;
